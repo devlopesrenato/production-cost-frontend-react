171011f36bf1b3591284ee4fe8aeb7b3cbe2c272
devLopesRenato <renatolopes37@gmail.com>
2024-02-27 23:07:28 -0300

    feat: config vite build

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,22 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: [
+            'react',
+            'react-dom',
+            'react-icons',
+            'react-redux',
+            'redux',
+            'antd',
+            'styled-components'
+          ],
+        },
+      },
+    },
+  },
 })
