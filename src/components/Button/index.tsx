1f97ba51ac0624d22f2d9237c4d63a55578a3e56
devLopesRenato <renatolopes37@gmail.com>
2024-03-26 00:42:53 -0300

    feat: improves the style of the button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { SButton } from "./styled";
+import Spinner from "../Spinner";
 
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   icon?: JSX.Element;
   title?: string;
+  styleType?: 'primary' | 'secondary';
   onClick?: () => void;
+  disabled?: boolean;
+  loading?: boolean;
+  style?: any;
 }
 
-const Button: React.FC<ButtonProps> = ({ icon, title, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ icon, title, styleType, style, disabled, loading, onClick, ...rest }) => {
   return (
-    <SButton onClick={() => onClick && onClick()}>
+    <SButton
+      {...rest}
+      $type={styleType}
+      style={style}
+      onClick={() => onClick && onClick()}
+      disabled={disabled}
+    >
+      {loading ? <Spinner size={18} /> : <></>}
       {title ? title : ""}
       {icon ? icon : <></>}
     </SButton>
--- src/components/Button/styled.ts
+++ src/components/Button/styled.ts
@@ -1,17 +1,31 @@
 import styled from "styled-components";
 
-const SButton = styled.div`
-    background-color: ${({ theme }) => theme.colors.button};
-    padding: 0.3rem;
+const SButton = styled.button<{ $type?: string }>`
     display: flex;
-    flex-direction: row-reverse;
     align-items: center;
-    border-radius: 8px;
-    gap: 0.3rem;
+    justify-content: center;
+    gap: 5px;
+    font-size: 14px;
+    height: 32px;
+    padding: 4px 15px;
+    border-radius: 6px;
+    outline: 0;
+    cursor: pointer;
+    border: 1px solid;
+    border-color: ${({ $type, theme }) => $type === 'primary' ? '#1677ff' : theme.colors.backgroundHeadTable};
+    background-color: ${({ $type }) => $type === 'primary' ? '#1677ff' : '#FFF'};
+    color: ${({ $type }) => $type === 'primary' ? '#FFF' : '#000'};
     &:hover {
-        background-color: ${({ theme }) => theme.colors.buttonHover};
-        cursor: pointer; 
+        border-color: #1677ff;
+        color: ${({ $type }) => $type === 'primary' ? '#FFF' : '#1677ff'};
+        background-color: ${({ $type }) => $type === 'primary' ? '#1677ff5e' : '#FFFFFFd0'};
+    }
+    &:disabled {
+        cursor: not-allowed;
+        background-color: ${({ $type }) => $type === 'primary' ? '#1677ffd0' : '#FFFFFFd0'};
+        border-color: ${({ $type, theme }) => $type === 'primary' ? '#1677ff' : theme.colors.backgroundHeadTable};
+        color: ${({ $type }) => $type === 'primary' ? '#FFF' : '#000'};
     }
 `
 
-export { SButton}
+export { SButton }
