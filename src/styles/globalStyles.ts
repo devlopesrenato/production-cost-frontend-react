427d2f8548882b5f22489becaba0bf1938976888
devLopesRenato <renatolopes37@gmail.com>
2024-08-30 22:06:18 -0300

    feat: style adjustments

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -5,6 +5,7 @@ const globalStyles = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
     }
     
     body, #root {
